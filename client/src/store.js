3e050173f24aadde1c0be0010474cf47ae79025f
yash gupta <yash3144@gmail.com>
2021-08-30 22:05:59 +0530

    Create store

---
+++ client/src/store.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { getwordReducer } from './reducers/wordReducers';
+
+
+
+
+const finalReducer = combineReducers({
+    getwordReducer: getwordReducer
+});
+
+const currentWord = localStorage.getItem('currentWord') ? JSON.parse(localStorage.getItem('currentWord')) : null;
+
+const initialState = {
+    // cartReducer: {
+    //     cartItems: cartItems
+    // },
+    getwordReducer: {
+        currentWord: currentWord
+    }
+};
+const composeEnhancers = composeWithDevTools({});
+
+const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
