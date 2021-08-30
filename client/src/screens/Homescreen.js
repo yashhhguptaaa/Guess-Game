fed499341cacc4d22618559b23c6dcecaebbe2db
yash gupta <yash3144@gmail.com>
2021-08-30 22:06:34 +0530

    Add proxy and install axios

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Input, Box, Flex, Spacer, Select, Button, chakra } from "@chakra-ui/react";
-
+import { getrandomword } from '../actions/wordActions';
 
 export default function Homescreen() {
+
+    const dispatch = useDispatch()
+    function getword(){
+        dispatch(getrandomword());
+    }
+
     return (
         <div>
             <Box bgGradient="linear(to-t, gray.200, gray.500)" boxShadow="dark-lg" p="5" maxW="500px" borderWidth="5px" alignContent="center" m="auto" mt="40px" mb="40px">
                 <chakra.h1 fontSize="48px" fontWeight= "bold">Are You Excited For The Game🥳🥳🥳</chakra.h1>
                 <Button
-                    colorScheme="teal" variant="outline"
+                    colorScheme="teal" variant="outline" onClick={getword}
                 >
                     Let's Guess
                 </Button>
