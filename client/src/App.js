0ced6a92d119b4dddb91f962483e576ea895e4d0
yash gupta <yash3144@gmail.com>
2021-08-30 21:25:22 +0530

    Create the Homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter , Route ,Link ,Switch} from 'react-router-dom';
+import Homescreen from './screens/Homescreen';
+
 
 function App() {
   return (
     <div className="App">
-      Home Page
+      <BrowserRouter>
+        <Route path="/" exact component={Homescreen} />
+        {/* <Route path="/details" exact component={Detailsscreen} /> */}
+      </BrowserRouter>
     </div>
   );
 }
