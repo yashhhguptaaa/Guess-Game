9a29b476628d0558a6f70958eccd6c854192efc9
yash gupta <yash3144@gmail.com>
2021-08-30 21:43:19 +0530

    Create normal UI

---
+++ client/src/actions/wordActions.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const getrandomword = () => async (dispatch,getdata) => {
+
+    dispatch({type : 'GET_WORD_REQUEST'});
+
+    try {
+        console.log("Inside Post")
+        const response = await axios.get('/api/words/getrandomword');
+        console.log(response);
+        dispatch({type : 'GET_WORD_SUCCESS',payload:response.data})
+
+        const currentWord = getdata().getwordReducer.user;
+        localStorage.setItem('currentUser',JSON.stringify(currentWord));
+        window.location.href='/game'
+
+    } catch (error) {
+        
+        dispatch({type : 'GET_WORD_FAILED' , payload : error});
+
+    }
+
+}
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Input, Box, Flex, Spacer, Select, Button, chakra } from "@chakra-ui/react";
+
 
 export default function Homescreen() {
     return (
         <div>
-            Home Screen
+            <Box bgGradient="linear(to-t, gray.200, gray.500)" boxShadow="dark-lg" p="5" maxW="500px" borderWidth="5px" alignContent="center" m="auto" mt="40px" mb="40px">
+                <chakra.h1 fontSize="48px" fontWeight= "bold">Are You Excited For The Game🥳🥳🥳</chakra.h1>
+                <Button
+                    colorScheme="teal" variant="outline"
+                >
+                    Let's Guess
+                </Button>
+            </Box>
         </div>
     )
 }
