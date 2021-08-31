a8efd5776fa58f609efd42f4cde650d5323243cd
yash gupta <yash3144@gmail.com>
2021-08-31 20:31:39 +0530

    import path file for deployment

--- server.js
+++ server.js
@@ -2,23 +2,22 @@ const express = require("express");
 const db= require('./db');
 const wordRoute = require('./routes/wordRoute');
 
-// const path = require('path')
+const path = require('path')
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/words',wordRoute);
-// app.use('/api/users',userRoutes);
-// app.use('/api/orders',orderRoutes);
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use('/',express.static('client/build'))
 
-//     app.get('*' , (req,res) => {
+if(process.env.NODE_ENV === 'production'){
+    app.use('/',express.static('client/build'))
 
-//         res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-//     })
-// }
+    app.get('*' , (req,res) => {
+
+        res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
+    })
+}
 
 // app.get('/',(req,res)=> {
 //     res.send("Server Working 🔥");
